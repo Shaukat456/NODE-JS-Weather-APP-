849a8040e50908e09cb1211ea2b5b3c4bbc5b7f5
Shaukat <sohailriazz267@gmail.com>
2020-12-27 12:05:41 +0500

    Initial Commit

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
-const http=require('http')
-const fs=require('fs')
-var requests=require('requests')
+const http = require('http')
+const fs = require('fs')
+var requests = require('requests')
 
 
 
-const homeFile=fs.readFileSync('Home.html','utf-8');
+const homeFile = fs.readFileSync('Home.html', 'utf-8');
 
+// defining replace value function
 
-const server=http.createServer((req,res)=>{
-    if(req.url=='/'){
+const replaceVal = (tempValue, orginalValue) => {
+    let temperature = tempValue.replace('{%tempval%}', orginalValue.current.temp_c)
+ temperature = temperature.replace('{%tempMin%}', orginalValue.current.temp_c)
+ temperature = temperature.replace('{%tempMax%}', orginalValue.current.temp_c)
+ temperature = temperature.replace('{%location%}', orginalValue.current.temp_c)
+ temperature = temperature.replace('{%country%}', orginalValue.current.temp_c)
+    console.log(temperature)
+}
+
+const server = http.createServer((req, res) => {
+    if (req.url == '/') {
         requests('http://api.weatherapi.com/v1/current.json?key=37e99908a19c49478dd130303202612&q=Karachi')
-.on('data', function (chunk) {
-  console.log(chunk)
-})
-.on('end', function (err) {
-  if (err) return console.log('connection closed due to errors', err);
- 
-  console.log('end');
-});
+            .on('data', function (chunk) {
+                const obJdata = JSON.parse(chunk)
+                const arrdata = [obJdata]
+                console.log(arrdata[0].current.temp_c)
+
+                const RealTimeDate = arrdata.map((value) => {
+                   console.log(value.current)
+                    // replaceVal(homeFile, value)
+                })
+
+            })
+            .on('end', function (err) {
+                if (err) return console.log('connection closed due to errors', err);
+
+                console.log('end');
+            });
     }
 })
 
 
-server.listen(80,'listening to port 80')
+server.listen(80, '127.0.0.1')
